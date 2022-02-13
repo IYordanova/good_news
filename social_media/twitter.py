97b49133221d95f997347e66522608dc20cffb98
IYordanova <ivs.yordanova@gmail.com>
2022-02-13 23:31:44 +0000

    twitter auth fix

--- social_media/twitter.py
+++ social_media/twitter.py
@@ -9,16 +9,25 @@ class Twitter:
 
     def __get_links(self, bucket_name, prefix):
         gcp_client = storage.Client()
-        blobs = gcp_client.get(bucket_name, prefix=prefix)
+        blobs = list(gcp_client.list_blobs(bucket_name, prefix=prefix))
         return blobs[0].metadata['link']
 
     def post(self, files_to_post):
-        logging.info(f"Posting {len(files_to_post)} links")
+        pre_post = "\n".join(files_to_post)
+        logging.info(f"Tweeting {len(files_to_post)} links: {pre_post}")
         bucket_name = os.getenv("ARCHIVE_BUCKET")
-        auth = os.getenv("BEARER_TOKEN")
 
         tweet = '\n'.join(map(lambda f: self.__get_links(bucket_name, f), files_to_post))
 
-        client = tweepy.Client(bearer_token=auth)
-        status = client.create_tweet(text=tweet)
-        logging.info(status.data)
+        auth = tweepy.OAuthHandler(
+            consumer_key=os.getenv("CONSUMER_KEY"),
+            consumer_secret=os.getenv("CONSUMER_SECRET_KEY")
+        )
+        auth.set_access_token(
+            os.getenv("ACCESS_TOKEN"),
+            os.getenv("ACCESS_TOKEN_SECRET")
+        )
+        api = tweepy.API(auth)
+        status = api.update_status(tweet)
+
+        logging.info(status)
